5a49624cc1b1270e5ec76f9304fa1750b63a804b
Shibaji Chakraborty <chakras4@ldb-6yrfq54.dbwks.erau.edu>
2025-12-25 23:56:33 -0500

    Updated plots

--- py/fti.py
+++ py/fti.py
@@ -8,6 +8,9 @@ from pynasonde.vipir.ngi.source import DataSource
 from pynasonde.vipir.ngi.plotlib import Ionogram
 from scipy.interpolate import griddata
 
+import sys
+sys.path.append("./py/")
+import fetch
 
 def generate_fti_profiles(folder, fig_title="", stn="", flim=[3.5, 4.5]):
     import os
@@ -15,7 +18,7 @@ def generate_fti_profiles(folder, fig_title="", stn="", flim=[3.5, 4.5]):
     from loguru import logger
 
     ds = DataSource(source_folder=folder)
-    ds.load_data_sets(30, 150)
+    ds.load_data_sets()
     df = 0.3
     for f in np.round(np.arange(2, 3.5, df), 1):
         os.makedirs(f"tmp/fti/{f}", exist_ok=True)
@@ -26,86 +29,10 @@ def generate_fti_profiles(folder, fig_title="", stn="", flim=[3.5, 4.5]):
             xtick_locator=mdates.MinuteLocator(interval=10),
             date_format=r"$%H^{%M}$",
             xdate_lims=[
-                dt.datetime(2022, 8, 24, 1),
-                dt.datetime(2022, 8, 24, 2),
+                dt.datetime(2022, 8, 22, 1),
+                dt.datetime(2022, 8, 22, 2),
             ],
         )
-        pass
-    # rti = ds.extract_FTI_RTI(
-    #     rlim=[95, 110],
-    # )
-    # fname = f"{ds.datasets[0].URSI}_{rti.time.min().strftime('%Y%m%d.%H%M-')}{rti.time.max().strftime('%H%M')}_O-mode.png"
-    # fig_title = f"""{ds.datasets[0].URSI}/01-02 UT, {rti.time.max().strftime('%d %b %Y')}"""
-    # i = Ionogram(fig_title=fig_title, nrows=1, ncols=1)
-    # ax = i._add_axis(False)
-    # # ax.set_xlim(xlim)
-    # ax.set_xlabel("Time, UT", fontdict={"size": i.font_size})
-    # ax.set_ylim([95, 110])
-    # ax.set_ylabel("Virtual Height, km", fontdict={"size": i.font_size})
-    # hours = mdates.HourLocator(interval=1)
-    # ax.xaxis.set_major_locator(hours)
-    # ax.xaxis.set_major_formatter(DateFormatter(r"$%H^{%M}$"))
-    # X, Y, Z = utils.get_gridded_parameters(
-    #     rti,
-    #     xparam="time",
-    #     yparam="range",
-    #     zparam="frequency",
-    #     rounding=False,
-    # )
-    # # Mask values above 3.5 MHz
-    # Z[Z > 3.5] = np.nan
-
-    # # Spline fill NaNs in Z (X is datetime, so convert to float for interpolation)
-
-    # # Flatten the grids for interpolation
-    # X_flat = mdates.date2num(X.flatten())
-    # Y_flat = Y.flatten()
-    # Z_flat = Z.flatten()
-
-    # # Find valid (non-nan) points
-    # mask = ~np.isnan(Z_flat)
-    # points = np.column_stack((X_flat[mask], Y_flat[mask]))
-    # values = Z_flat[mask]
-
-    # # Interpolate at nan locations
-    # nan_mask = np.isnan(Z_flat)
-    # interp_points = np.column_stack((X_flat[nan_mask], Y_flat[nan_mask]))
-
-    # if interp_points.size > 0 and points.size > 0:
-    #     Z_flat[nan_mask] = griddata(points, values, interp_points, method='cubic')
-
-    # # Reshape back to original grid
-    # Z = Z_flat.reshape(Z.shape)
-    # im = ax.contourf(
-    #     X,
-    #     Y,
-    #     Z.T,
-    #     cmap="jet_r",
-    #     alpha=0.4,
-    #     vmin=0, vmax=3.5,
-    #     levels=np.arange(0, 3.5, .5),
-    # )
-    # i._add_colorbar(im, i.fig, ax, label=r"$f_0$, MHz")
-    # cs = ax.contour(
-    #     X,
-    #     Y,
-    #     Z.T,
-    #     levels=np.arange(0, 3.5, .5),
-    #     colors='k',
-    #     linewidths=0.2,
-    #     zorder=5,
-    # )
-    # # Optionally label the contour lines
-    # ax.clabel(cs, inline=True, fontsize=i.font_size * 0.5)
-    # ax.set_xlim(dt.datetime(2022, 8, 22, 1), dt.datetime(2022, 8, 22, 2))
-    # hours = mdates.MinuteLocator(interval=10)
-    # ax.xaxis.set_major_locator(hours)
-    # ax.xaxis.set_major_formatter(DateFormatter(r"$%H^{%M}$"))
-    # ax.axvline(dt.datetime(2022, 8, 22, 1, 16), ls="--", lw=0.5, color="k")
-
-    # i.save(os.path.join("tmp", fname))
-    # i.close()
-
 
     mode = "O"
     rti = pd.DataFrame()
@@ -157,8 +84,8 @@ def generate_fti_profiles(folder, fig_title="", stn="", flim=[3.5, 4.5]):
         del_ticks = False,
         xtick_locator = mdates.MinuteLocator(interval=10),
         xdate_lims = [
-            dt.datetime(2022, 8, 24, 1),
-            dt.datetime(2022, 8, 24, 2),
+            dt.datetime(2022, 8, 22, 1),
+            dt.datetime(2022, 8, 22, 2),
         ],
     )
     ax.text(
@@ -168,9 +95,9 @@ def generate_fti_profiles(folder, fig_title="", stn="", flim=[3.5, 4.5]):
         0.95, 1.05, r"(A) $f_0$=[2.0-2.3] MHz", ha="right", va="center", transform=ax.transAxes
     )
     ax.set_xticklabels([])
-    ax.axvline(dt.datetime(2022, 8, 24, 1, 16), ls="--", lw=0.5, color="k")
-    ax.axvline(dt.datetime(2022, 8, 24, 1, 17, 26), ls="--", lw=1.5, color="red")
-    ax.axvline(dt.datetime(2022, 8, 24, 1, 21, 9), ls="--", lw=1.5, color="red")
+    ax.axvline(dt.datetime(2022, 8, 22, 1, 16), ls="--", lw=0.5, color="k", zorder=5)
+    ax.axvline(dt.datetime(2022, 8, 22, 1, 17, 26), ls="--", lw=1.5, color="red", zorder=5)
+    ax.axvline(dt.datetime(2022, 8, 22, 1, 21, 9), ls="--", lw=1.5, color="red", zorder=5)
 
     flim = [2.9, 3.2]
     o = rti[
@@ -192,16 +119,16 @@ def generate_fti_profiles(folder, fig_title="", stn="", flim=[3.5, 4.5]):
         add_cbar = True,
         xtick_locator = mdates.MinuteLocator(interval=10),
         xdate_lims = [
-            dt.datetime(2022, 8, 24, 1),
-            dt.datetime(2022, 8, 24, 2),
+            dt.datetime(2022, 8, 22, 1),
+            dt.datetime(2022, 8, 22, 2),
         ],
     )
     ax.text(
         0.95, 1.05, fr"(B) $f_0$=[{flim[0]}-{flim[1]}] MHz", ha="right", va="center", transform=ax.transAxes
     )
-    ax.axvline(dt.datetime(2022, 8, 24, 1, 16), ls="--", lw=0.5, color="k")
-    ax.axvline(dt.datetime(2022, 8, 24, 1, 17, 26), ls="--", lw=1.5, color="red")
-    ax.axvline(dt.datetime(2022, 8, 24, 1, 21, 9), ls="--", lw=1.5, color="red")
+    ax.axvline(dt.datetime(2022, 8, 22, 1, 16), ls="--", lw=0.5, color="k", zorder=5)
+    ax.axvline(dt.datetime(2022, 8, 22, 1, 17, 26), ls="--", lw=1.5, color="red", zorder=5)
+    ax.axvline(dt.datetime(2022, 8, 22, 1, 21, 9), ls="--", lw=1.5, color="red", zorder=5)
 
     
     i.save(os.path.join("tmp", f"jgr.png"))
@@ -210,14 +137,24 @@ def generate_fti_profiles(folder, fig_title="", stn="", flim=[3.5, 4.5]):
 
 
 ## Analyzing the dataset form Speed Deamon 2022
-for doy in range(236, 237, 1):
+for doy in range(234, 235, 1):
     stn = "WI937"
     date = dt.datetime(2022, 1, 1) + dt.timedelta(days=doy - 1)
     print(date)
     fig_title = f"Speed Demon / {date.strftime('%Y-%m-%d')}"
 
+    local, remote = fetch.create_local_folder(date=date)
+    fetch.copy2local(
+        local=local,
+        remote_files=fetch.get_ngi_files_by_hour(
+            date=date,
+            hours=[1, 2],
+            remote=remote,
+        ),
+    )
+
     generate_fti_profiles(
-        folder=f"/tmp/{doy}/",
+        folder=local,
         fig_title=fig_title,
         stn=stn,
         flim=[2.5, 3.5],
